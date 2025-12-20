447247b1eb20e91fb2aa4d1d1c51a8537fc57f6a
Matthew Maxwell <maxwellmattryan@gmail.com>
2025-12-19 18:04:52 -0600

    Add pulsing icon to device items

--- src/lib/components/devices/DeviceItem.svelte
+++ src/lib/components/devices/DeviceItem.svelte
@@ -31,5 +31,8 @@
 	</div>
 
 	<!-- Connected indicator -->
-	<div class="h-2 w-2 shrink-0 rounded-full bg-emerald-500" title="Connected"></div>
+	<span class="relative flex size-2" title="Connected">
+		<span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-emerald-400 opacity-75"></span>
+		<span class="relative inline-flex size-2 rounded-full bg-emerald-500"></span>
+	</span>
 </div>
